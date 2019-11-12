97f142bef45bfe65b73a4a54238c1ad3775fdd30
Somerlinck <leo.vuylsteker@imt-atlantique.net>
2019-11-12 12:26:36 +0100

    hnn implementation

--- src/hnn/Neuron.java
+++ src/hnn/Neuron.java
@@ -1,21 +1,71 @@
 package hnn;
 
 public class Neuron {
-	private float u; // internal state
-	private int v; // external state
+	private double u; // internal state
+	private double v; // external state
+	int x; // row
+	int i; // column
 	
 	// default
-	public Neuron() {
-		this.u = 0;
-		this.v = 0;
+	public Neuron(int i, int j, double u_init) {
+		this.u = u_init;
+		this.v = Math.random();
+		this.x = i;
+		this.i = j;
 	}
 	
-	public float getU() {
+	public double getU() {
 		return this.u;
 	}
 	
-	public int getV() {
+	public double getV() {
 		return this.v;
+	}
+	
+	public void update(int A, int B, int C, int D, float T, float delta, Neuron[][] network, DistanceMatrix d) {
+		int n = network.length;
+		
+		//	number of 1s per row
+		int sum_a = 0;
+		for(int j=0; j < n; j++) {
+			if(j != this.i) {
+				sum_a += network[this.x][j].getV();
+			}
+		}
+		
+		// number of 1s per column
+		int sum_b = 0;
+		for(int y = 0; y < n; y++) {
+			sum_b += network[y][this.i].getV();
+		}
+		
+		// number of 1s
+		int sum_c = -n;
+		for(int k = 0; k < n; k++) {
+			for(int l = 0; l < n; l++) {
+				sum_c += network[k][l].getV();
+			}
+		}
+		
+		// distance from neighbors
+		int sum_d = 0;
+		for(int y = 0; y < n; y++) {
+			if(this.i == 0) {
+				sum_d += d.get(this.x, y) * (network[y][this.i + 1].getV() + network[y][n - 1].getV());
+			}
+			if(this.i == n-1) {
+				sum_d += d.get(this.x, y) * (network[y][0].getV() + network[y][this.i - 1].getV());
+			}
+			else{
+				sum_d += d.get(this.x, y) * (network[y][this.i + 1].getV() + network[y][this.i - 1].getV());
+			}
+		}
+		
+		//	update u
+		this.u = this.getU() + delta * (-this.getU() -A * sum_a -B * sum_b -C * sum_c -D * sum_d);
+		
+		//	update v
+		this.v = 1/2 * (1 + Math.tanh(this.getU()/T));
 	}
 
 }
